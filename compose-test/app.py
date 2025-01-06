f653ddf60bb4eaf7a505d4290ffa37757f70e3bc
nagata <hgtm.kabocha@gmail.com>
2025-01-07 08:04:03 +0900

    Add redis

--- compose-test/app.py
+++ compose-test/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import redis
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def index():
-    return 'Hello, World!\n'
+    count = cache.incr('hits')
+    return 'Hello, World! I have been seen {} times\n'.format(count)
